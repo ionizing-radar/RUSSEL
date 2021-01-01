1b97e8ce28d2349db59513ab68741b60c92c5aa8
kerma <kerma@ionizing-radar.ca>
2021-01-01 23:02:35 +0000

    simple echo connect to Russel

--- app/src/main/java/com/example/russel/NetworkConnection.java
+++ app/src/main/java/com/example/russel/NetworkConnection.java
@@ -1,6 +1,8 @@
 package com.example.russel;
 
 import android.location.GnssAntennaInfo;
+import android.net.wifi.EasyConnectStatusCallback;
+import android.telecom.Call;
 import android.util.EventLog;
 import android.util.Log;
 
@@ -19,17 +21,39 @@ import java.net.Socket;
 import java.util.EventListener;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NetworkConnection {
 
+    private class GetSocket implements Runnable {
+        @Override
+        public void run() {
+            try {
+                socket = new Socket (ip, port);
+                Log.i(GetSocket.class.getName(),"got socket");
+            } catch (Exception e) {
+                Log.i(GetSocket.class.getName(),"Error :" + e.toString());
+            }
+        }
+    }
+
+
     private static final String TAG = NetworkConnection.class.getName();
+    private final ExecutorService threadPool;
 
     private InetAddress ip;
     private int port;
+    private Socket socket = null;
 
     public NetworkConnection(InetAddress ip, int port) {
         this.ip = ip;
         this.port = port;
+
+        threadPool = Executors.newFixedThreadPool(1);
+        threadPool.execute(new GetSocket());
     }
 
     public <T> void sendMessage(T message) {
